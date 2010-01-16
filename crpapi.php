e0ea9c1fa5382ecd74c136d7cd91f915ceb16004
= <bpilkerton@gmail.com>
2010-01-16 16:44:47 -0500

    Added file cache mechanism.  Improved example usage.

--- crpapi.php
+++ crpapi.php
@@ -1,16 +1,17 @@
 <?php
 /**
  * class php-crpapi
- * Simple PHP client library for working with the Center for Responsive Politics' API
+ * Simple PHP client library for working with the Center for Responsive Politics' API.
  * Information on CRP's API can be found at http://www.opensecrets.org/action/api_doc.php
  * Information on using this class, including examples at http://github.com/bpilkerton/php-crpapi
  * @author Ben Pilkerton <bpilkerton@gmail.com>
- * @version 0.1
+ * @version 0.2
  */
 
 class crpData {
 
 	function __construct($method=NULL,$params=NULL) {
+
 		$this->apikey = "";
 		$this->baseurl = "http://api.opensecrets.org/";
 		$this->output = "json";
@@ -19,35 +20,66 @@ class crpData {
 		$this->output = isset($params['output']) ? $params['output']: $this->output;
 		$this->method = $method;
 		self::loadParams($params);
+		
+		$this->fileHash = md5($method . "," . implode(",",$params));
+		$this->cacheHash = "dataCache/" . $this->fileHash;
+		$this->cacheTime = 86400; #one day
+		
 	}
 
 	private function loadParams($params) {
 		$this->url = $this->baseurl . "?method=" . $this->method . 
 			"&apikey=" . $this->apikey;
-		
+
 		foreach ($params as $key=>$val) {
 			$this->url .= "&" . $key . "=" . $val;
 			$this->$key = $val;
 		}
-		
+
 		return;
 	}
 	
-	public function getData() {
+	public function getData($useCache=true) {
 	
-		$this->rawdata = file_get_contents($this->url);
+		if ($useCache and file_exists($this->cacheHash) and (time() - filectime($this->cacheHash) < $this->cacheTime)) {
 		
-		switch ($this->output) {
-			case "json":
-				$this->data = json_decode($this->rawdata);
-				break;
-			case "xml":
-				$this->data = simplexml_load_string($this->rawdata);
-				break;
+			$this->cacheHit = true;
+			$file = fopen($this->cacheHash,"r");
+			$this->data = stream_get_contents($file);
+			$this->data = gzuncompress($this->data);
+			$this->data = unserialize($this->data);
+			fclose($file);
+
+		} else {
+			$this->cacheHit = false;
+			$this->data = file_get_contents($this->url);
+
+			switch ($this->output) {
+				case "json":
+					$this->data = json_decode($this->data,true);
+					break;
+				case "xml":
+					$this->data = simplexml_load_string($this->data);
+					break;
+				default:
+					die("Unknown output type.  Use 'json' or 'xml'");
+			}
+
+			if ($useCache) {
+				$file = fopen($this->cacheHash,"w");
+				$store = serialize($this->data);
+				$store = gzcompress($store);
+				fwrite($file,$store);
+				fclose($file);
+			}
 		}
-	
+		
 		return $this->data;
 	}
-}
+	
+	function getCacheStatus() {
+		return $this->cacheHit;
+	}
 
+}
 ?>
--- example.php
+++ example.php
@@ -4,11 +4,22 @@
  * php-crpapi example usage
  * @author Ben Pilkerton
  *
+ *
+ * There are a few options which need to be defined in the constructor 
+ * in crpapi.php.  These include specifying your API key, default 
+ * output type (json recommended, default) and cache lifetime 
+ * (one day is default).
+ *
+ * Include the CRP API Library and create an object instance, specifying
+ * the parameters.  In this example, we specify the output type.  This
+ * could be ommitted and the default (defined in the constructor) will
+ * be used.
+ *
 **/
 
 require_once('crpapi.php');
 
-$crp = new crpData("candIndustry", Array("cid"=>"N00000019","cycle"=>"2006","output"=>"xml"));
+$crp = new crpData("candIndustry", Array("cid"=>"N00002408","cycle"=>"2010","output"=>"json"));
 
 /**
  * These variables are exposed upon instantiation
@@ -17,21 +28,49 @@ $crp = new crpData("candIndustry", Array("cid"=>"N00000019","cycle"=>"2006","out
 echo $crp->apikey . "<br />";
 echo $crp->output . "<br />";
 echo $crp->method . "<br />";
+echo $crp->baseurl    . "<br />";
 echo $crp->url    . "<br />";
-
-/**
- * Get the data (either JSON std obj or SimpleXML Object)
-**/
-
 echo "<hr />";
 
-$data = $crp->getData();
-print_r($data);
-
 /**
- * Alternatively use the raw response after getData() is run
+ * Get the data. This example retrieves json data which is converted to 
+ * an associative array. If using xml, a SimpleXML object will be returned.  
+ * The getData method can optionally be passed a true/false value (true is 
+ * default).  If set to false, a local file cache will not be used.
 **/
 
-print_r($crp->rawdata);
+$data = $crp->getData();
+
+//Metadata
+foreach ($data['response']['industries']['@attributes'] as $key=>$val) {
+	echo $key . " => " . $val . "<br />";
+}
+
+//Table Data
+echo "<table><tr><th>Industry</th><th>Indivs</th><th>PACs</th><th>Total</th></tr>";
+foreach ($data['response']['industries']['industry'] as $ind) {
+	foreach ($ind as $row) {
+		echo "<tr><td>" . $row['industry_name'] . "</td><td>$" . 
+			$row['indivs'] . "</td><td>$" . $row['pacs'] . "</td><td>$" . 
+			$row['total'] . "</td></tr>";
+	}
+}
+
+echo "</table>";
+echo "<hr />";
+
+/**
+ * Show the cache status.  By default, the library caches API query results in a
+ * gzipped, serialized form in a text file in the dataCache directory.  If you do 
+ * not desire file caching, call getData(false) (see above).  The cache life can
+ * be set by altering $this->cacheTime value in crpapi.php.  The default is 
+ * one day.
+**/
+
+if ($crp->getCacheStatus()) {
+	echo "Cache Hit";
+} else {
+	echo "Cache Miss";
+}
 
 ?>
